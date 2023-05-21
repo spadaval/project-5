2f9c563df106ed81d337e19b933f436b1ed5104d
Sumeet Padavala <sumeet.padavala@gmail.com>
2023-05-20 17:03:13 -0700

    Switch to brute-force

--- project5/SuffixArray.java
+++ project5/SuffixArray.java
@@ -1,10 +1,10 @@
 package project5;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -26,16 +26,27 @@ class Suffix {
 
 /**
  * An implementation of a bucket-based radix-sort.
- * Buckets are stored in a map to avoid allocating memory for characters that
- * aren't actually present.
  * If the string has no character at a particular index, it goes into the
  * `start` list. These strings will be placed before any other strings in the
  * returned output.
  */
 class Buckets {
-  LinkedList<Suffix> start = new LinkedList<>();
-  Map<Character, LinkedList<Suffix>> buckets = new HashMap<>();
+  // the index of the character in the string we should use to sort by
   int index = 0;
+  /**
+   * Elements that have no valid character at this point go here.
+   */
+  LinkedList<Suffix> start = new LinkedList<>();
+  /**
+   * Strings are placed in an appropriate bucket based on the character at
+   * position `index`.
+   * 
+   * We use a map to avoid allocating a new list for characters that don't exist
+   * in the string.
+   * The domain of keys is very restricted,
+   * so a treemap will still have O(1) operations in practice.
+   */
+  TreeMap<Character, LinkedList<Suffix>> buckets = new TreeMap<>();
 
   Buckets(int index) {
     this.index = index;
@@ -50,24 +61,30 @@ class Buckets {
       start.add(s);
   }
 
-  // should only be called once
+  /**
+   * Collapse the buckets into one sorted list of suffixes.
+   * 
+   * Note: This function leaves the instance in an invalid state.
+   * The instance should not be used after calling this function.
+   * 
+   * @return A sorted list of suffixes.
+   */
   public List<Suffix> get() {
-    // System.out.println(start);
-    // System.out.println(buckets);
-    List<Suffix> sortedBuckets = buckets.entrySet().stream()
-        .sorted((s1, s2) -> s1.getKey().compareTo(s2.getKey()))
-        .flatMap(it -> sort(it.getValue(), index + 1).stream())
-        .collect(Collectors.toList());
+    for (Character key : buckets.keySet()) {
+      start.addAll(sort(buckets.get(key), index + 1));
+    }
 
-    start.addAll(sortedBuckets);
     return start;
   }
 
+  /**
+   * Sort strings by the characters at position `index`.
+   * 
+   * @param strings The strings to sort
+   * @param index   The index of the character to sort by
+   * @return A list of sorted strings
+   */
   public static List<Suffix> sort(List<Suffix> strings, int index) {
-
-    // System.out.print("Sorting[index=" + index + "]: ");
-    // System.out.println(strings);
-
     int longestStringLength = strings.stream().map(it -> it.str.length()).max(Integer::compareTo).get();
     if (longestStringLength < index)
       return strings;
@@ -83,11 +100,14 @@ class Buckets {
 
 public class SuffixArray {
   public ArrayList<Integer> construct(String S) {
-    System.out.println(S);
-    List<Suffix> strings = IntStream.range(0, S.length()).boxed().map(i -> new Suffix(S.substring(i), i))
+    // System.out.println(S);
+    List<Suffix> strings = IntStream.range(0, S.length()).boxed()
+        .map(i -> new Suffix(S.substring(i), i))
         .collect(Collectors.toList());
 
-    strings = Buckets.sort(strings, 0);
+    // strings = Buckets.sort(strings, 0);
+    Collections.sort(strings, (a, b) -> a.str.compareTo(b.str));
+
     return strings.stream().map(it -> it.index).collect(Collectors.toCollection(ArrayList::new));
   }
 }
